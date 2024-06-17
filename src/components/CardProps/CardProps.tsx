2264dd6b42f0cb8cfa2367ca6772fc2f43d8b113
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-17 15:13:47 +0300

    1 Created default custom component CardProps.

---
+++ src/components/CardProps/CardProps.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CardProps.css';
+
+const CardProps = () => (
+  <div className='card-props'>
+    <h1>Hello wordl!</h1>
+    <h1>Hello wordl!</h1>
+  </div>
+);
+
+export default CardProps;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'the-new-css-reset/css/reset.css';
 import './index.css';
-import App from './App';
+import CardProps from './components/CardProps/CardProps';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CardProps></CardProps>
   </React.StrictMode>
 );
 
